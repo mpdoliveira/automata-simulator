3abd647d45885635df79b42362288ee3f7f0a395
Marcos Paulo <marcos.pdo1412@gmail.com>
2026-09-07 21:37:04 -0300

    refactor: Update automaton and state types to acomodate new feats

--- src/types.ts
+++ src/types.ts
@@ -1,11 +1,27 @@
+
+// types for readability
 export type Symbol = string;
 
 export type StateId = number;
 
+// types for eficient operation
 export type State = {
     label: string;
+    x: number;
+    y: number;
     transitions : Map<Symbol, Set<StateId>>;
 }
+
+export type Automaton = {
+    name : string;
+    alphabet?: Set<Symbol>;
+    states : Map<StateId, State>;
+    nextId : number;
+    initialStates : Set<StateId>;
+    finalStates : Set<StateId>;
+}
+
+// types for execution management
 
 export type Transition = {
     source : StateId;
@@ -19,15 +35,6 @@ export type Step = {
     before : StateId[];
     transitions : Transition[];
     after : StateId[];
-}
-
-export type Automaton = {
-    name : string;
-    alphabet?: Set<Symbol>;
-    states : Map<StateId, State>;
-    nextId : number;
-    initialStates : Set<StateId>;
-    finalStates : Set<StateId>;
 }
 
 export type RunResult = {
