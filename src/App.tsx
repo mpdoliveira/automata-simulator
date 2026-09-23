5408eabb7ddb308d53e2a1a9240ec92337da2421
Marcos Paulo <marcos.pdo1412@gmail.com>
2026-09-23 09:18:00 -0300

    feat: delete selected states in canvas

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import { useState } from "react";
 
-import { initAutomaton, addState, rmState } from "./engine/operations";
-import type { Automaton, StateId } from "./types";
+import {
+  initAutomaton,
+  addState,
+  rmState,
+  rmTransition,
+} from "./engine/operations";
+import type { Automaton, State, StateId, Transition } from "./types";
 
 import Button from "./components/Button.tsx";
 import Canvas from "./components/canvas.tsx";
@@ -9,28 +14,53 @@ import Canvas from "./components/canvas.tsx";
 export default function App() {
   const [automaton, setAutomaton] = useState(initAutomaton());
 
-  const [selectedStates, setSelected] = useState(new Set<StateId>)
+  const [selectedStates, setSelectedStates] = useState(new Set<StateId>());
+  const [selectedTransitions, setSelectedTransitions] = useState(
+    new Set<Transition>(),
+  );
 
   function handleAddState() {
     setAutomaton(addState(automaton));
   }
 
-  function handleSelectState(id : StateId) {
+  function handleSelectState(id: StateId) {
     let newSelectedStates = new Set(selectedStates);
     if (selectedStates.has(id)) {
       newSelectedStates.delete(id);
-      setSelected(newSelectedStates);
+      setSelectedStates(newSelectedStates);
+    } else {
+      newSelectedStates.add(id);
+      setSelectedStates(newSelectedStates);
     }
-    else {
-      newSelectedStates.add(id) 
-        setSelected(newSelectedStates);
+  }
+
+  function handleDelete() {
+    for (const id of selectedStates) {
+      setAutomaton(rmState(automaton, id));
     }
+    for (const transition of selectedTransitions) {
+      setAutomaton(
+        rmTransition(
+          automaton,
+          transition.source,
+          transition.symbol,
+          transition.target,
+        ),
+      );
+    }
+    setSelectedStates(new Set<StateId>());
+    setSelectedTransitions(new Set<Transition>());
   }
 
   return (
     <>
       <Button name="+ State" onClick={handleAddState} />
-      <Canvas automaton={automaton} selectedStates={selectedStates} onSelect={handleSelectState}/>
+      <Canvas
+        automaton={automaton}
+        selectedStates={selectedStates}
+        onSelect={handleSelectState}
+        onDelete={handleDelete}
+      />
     </>
   );
 }
--- src/components/canvas.tsx
+++ src/components/canvas.tsx
@@ -1,3 +1,5 @@
+import {} from "react";
+
 import type { Automaton, StateId } from "../types.tsx";
 
 import State from "./State.tsx";
@@ -5,24 +7,38 @@ import State from "./State.tsx";
 type Props = {
   automaton: Automaton;
   selectedStates: Set<StateId>;
-  onSelect: (id : StateId) => void;
+  onSelect: (id: StateId) => void;
+  onDelete: () => void;
 };
 
 function _() {}
 
-export default function Canvas({ automaton, selectedStates, onSelect}: Props) {
+export default function Canvas({
+  automaton,
+  selectedStates,
+  onSelect,
+  onDelete,
+}: Props) {
+  function handleKeyDown(e: React.KeyboardEvent<HTMLDivElement>) {
+    if (e.key === "Delete") {
+      onDelete();
+    }
+  }
+
   return (
-    <svg width="100%" height="70%">
-      {Array.from(automaton.states).map(([stateId, state]) => (
-        <State
-          label={state.label}
-          id={stateId}
-          isFinal={automaton.finalStates.has(stateId)}
-          isSelected={selectedStates.has(stateId)}
-          xCenter={stateId * 100 + 50}
-          onSelect={onSelect}
-        />
-      ))}
-    </svg>
+    <div tabIndex={0} onKeyDown={handleKeyDown}>
+      <svg width="100%" height="70%">
+        {Array.from(automaton.states).map(([stateId, state]) => (
+          <State
+            label={state.label}
+            id={stateId}
+            isFinal={automaton.finalStates.has(stateId)}
+            isSelected={selectedStates.has(stateId)}
+            xCenter={stateId * 100 + 50}
+            onSelect={onSelect}
+          />
+        ))}
+      </svg>
+    </div>
   );
 }
