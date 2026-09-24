7b15281df10695c4c4d93371f49cba951f89c465
Marcos Paulo <marcos.pdo1412@gmail.com>
2026-09-24 12:54:31 -0300

    feat: initial add transition by handles

--- src/components/canvas.tsx
+++ src/components/canvas.tsx
@@ -33,7 +33,15 @@ export default function Canvas({
 
   const sourceIdRef = useRef<StateId | null>(null);
 
-  
+  function handleTransition(stateId : StateId) {
+    if (sourceIdRef.current) {
+      onAddTransition(sourceIdRef.current, stateId);
+      sourceIdRef.current = null;
+    }
+    else {
+      sourceIdRef.current = stateId;
+    }
+  }
   
   return (
     <div tabIndex={0} onKeyDown={handleKeyDown}>
