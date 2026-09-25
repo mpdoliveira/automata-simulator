36ec096ad40ac412350a6ff6293159717b7e80d5
Marcos Paulo <marcos.pdo1412@gmail.com>
2026-09-25 19:10:45 -0300

    feat: arrowhead added to transition

--- src/components/Transition.tsx
+++ src/components/Transition.tsx
@@ -1,9 +1,9 @@
 import type { Position } from "../types";
 
 type Props = {
-  sourcePosition : Position;
+  sourcePosition: Position;
   symbol?: string;
-  targetPosition : Position;
+  targetPosition: Position;
 };
 
 export default function Transition({
@@ -11,14 +11,47 @@ export default function Transition({
   symbol = "",
   targetPosition,
 }: Props) {
+  const deltaX = targetPosition.x - sourcePosition.x;
+  const deltaY = targetPosition.y - sourcePosition.y;
+
+  const distance = Math.sqrt(
+    Math.pow(deltaX, 2) 
+    + Math.pow(deltaY, 2),
+  );
+
+  const unitX = deltaX / distance;
+  const unitY = deltaY / distance;
+
+  const offsetX = unitX * 30;
+  const offsetY = unitY * 30;
+
+  const sourceEdgePosition = {
+    x: sourcePosition.x + offsetX,
+    y: sourcePosition.y + offsetY,
+  }
+
+  const targetEdgePosition = {
+    x: targetPosition.x - offsetX,
+    y: targetPosition.y - offsetY,
+  }
+
   return (
     <>
+      <marker
+        id="arrow"
+        viewBox="0 0 10 10"
+        refX="10"
+        refY="5"
+        markerWidth="6"
+        markerHeight="6"
+        orient="auto-start-reverse"
+      ><path d="M 0 0 L 10 5 L 0 10 z" /></marker>
       <path
-        d={`M${sourcePosition.x},${sourcePosition.y} L${targetPosition.x},${targetPosition.y}`}
+        d={`M${sourceEdgePosition.x},${sourceEdgePosition.y} L${targetEdgePosition.x},${targetEdgePosition.y}`}
         stroke="black"
         strokeWidth="2"
+        markerEnd="url(#arrow)"
       />
-      <marker id="arrow" refX="40" refY="40" />
     </>
   );
 }
